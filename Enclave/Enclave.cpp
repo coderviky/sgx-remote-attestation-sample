98f1350b7086b1fd36abd94187207a21ee28d780
Vivek Suryawanshi <coderviky@gmail.com>
2023-08-19 18:43:29 +0530

    service provider private & pub key generation code added & instructions.txt added & sample is compiling executing successfuly as per instructions

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -27,23 +27,26 @@ in the License.
 #include <sgx_tkey_exchange.h>
 #include <sgx_tcrypto.h>
 
-static const sgx_ec256_public_t def_service_public_key = {
-    {
-        0x72, 0x12, 0x8a, 0x7a, 0x17, 0x52, 0x6e, 0xbf,
-        0x85, 0xd0, 0x3a, 0x62, 0x37, 0x30, 0xae, 0xad,
-        0x3e, 0x3d, 0xaa, 0xee, 0x9c, 0x60, 0x73, 0x1d,
-        0xb0, 0x5b, 0xe8, 0x62, 0x1c, 0x4b, 0xeb, 0x38
-    },
-    {
-        0xd4, 0x81, 0x40, 0xd9, 0x50, 0xe2, 0x57, 0x7b,
-        0x26, 0xee, 0xb7, 0x41, 0xe7, 0xc6, 0x14, 0xe2,
-        0x24, 0xb7, 0xbd, 0xc9, 0x03, 0xf2, 0x9a, 0x28,
-        0xa8, 0x3c, 0xc8, 0x10, 0x11, 0x14, 0x5e, 0x06
-    }
+// for generated public key
+#include "Pubkey.h"
 
-};
+// static const sgx_ec256_public_t def_service_public_key = {
+//     {
+//         0x72, 0x12, 0x8a, 0x7a, 0x17, 0x52, 0x6e, 0xbf,
+//         0x85, 0xd0, 0x3a, 0x62, 0x37, 0x30, 0xae, 0xad,
+//         0x3e, 0x3d, 0xaa, 0xee, 0x9c, 0x60, 0x73, 0x1d,
+//         0xb0, 0x5b, 0xe8, 0x62, 0x1c, 0x4b, 0xeb, 0x38
+//     },
+//     {
+//         0xd4, 0x81, 0x40, 0xd9, 0x50, 0xe2, 0x57, 0x7b,
+//         0x26, 0xee, 0xb7, 0x41, 0xe7, 0xc6, 0x14, 0xe2,
+//         0x24, 0xb7, 0xbd, 0xc9, 0x03, 0xf2, 0x9a, 0x28,
+//         0xa8, 0x3c, 0xc8, 0x10, 0x11, 0x14, 0x5e, 0x06
+//     }
 
-#define PSE_RETRIES	5	/* Arbitrary. Not too long, not too short. */
+// };
+
+#define PSE_RETRIES 5 /* Arbitrary. Not too long, not too short. */
 
 /*----------------------------------------------------------------------
  * WARNING
@@ -72,7 +75,7 @@ static const sgx_ec256_public_t def_service_public_key = {
  */
 
 /*
- * This doesn't really need to be a C++ source file, but a bug in 
+ * This doesn't really need to be a C++ source file, but a bug in
  * 2.1.3 and earlier implementations of the SGX SDK left a stray
  * C++ symbol in libsgx_tkey_exchange.so so it won't link without
  * a C++ compiler. Just making the source C++ was the easiest way
@@ -89,7 +92,7 @@ sgx_status_t get_report(sgx_report_t *report, sgx_target_info_t *target_info)
 }
 
 #ifdef _WIN32
-size_t get_pse_manifest_size ()
+size_t get_pse_manifest_size()
 {
 	return sizeof(sgx_ps_sec_prop_desc_t);
 }
@@ -97,17 +100,21 @@ size_t get_pse_manifest_size ()
 sgx_status_t get_pse_manifest(char *buf, size_t sz)
 {
 	sgx_ps_sec_prop_desc_t ps_sec_prop_desc;
-	sgx_status_t status= SGX_ERROR_SERVICE_UNAVAILABLE;
-	int retries= PSE_RETRIES;
+	sgx_status_t status = SGX_ERROR_SERVICE_UNAVAILABLE;
+	int retries = PSE_RETRIES;
 
-	do {
-		status= sgx_create_pse_session();
-		if ( status != SGX_SUCCESS ) return status;
+	do
+	{
+		status = sgx_create_pse_session();
+		if (status != SGX_SUCCESS)
+			return status;
 	} while (status == SGX_ERROR_BUSY && retries--);
-	if ( status != SGX_SUCCESS ) return status;
+	if (status != SGX_SUCCESS)
+		return status;
 
-	status= sgx_get_ps_sec_prop(&ps_sec_prop_desc);
-	if ( status != SGX_SUCCESS ) return status;
+	status = sgx_get_ps_sec_prop(&ps_sec_prop_desc);
+	if (status != SGX_SUCCESS)
+		return status;
 
 	memcpy(buf, &ps_sec_prop_desc, sizeof(ps_sec_prop_desc));
 
@@ -118,44 +125,52 @@ sgx_status_t get_pse_manifest(char *buf, size_t sz)
 #endif
 
 sgx_status_t enclave_ra_init(sgx_ec256_public_t key, int b_pse,
-	sgx_ra_context_t *ctx, sgx_status_t *pse_status)
+							 sgx_ra_context_t *ctx, sgx_status_t *pse_status)
 {
 	sgx_status_t ra_status;
 
 	/*
-	 * If we want platform services, we must create a PSE session 
+	 * If we want platform services, we must create a PSE session
 	 * before calling sgx_ra_init()
 	 */
 
 #ifdef _WIN32
-	if ( b_pse ) {
-		int retries= PSE_RETRIES;
-		do {
-			*pse_status= sgx_create_pse_session();
-			if ( *pse_status != SGX_SUCCESS ) return SGX_ERROR_UNEXPECTED;
+	if (b_pse)
+	{
+		int retries = PSE_RETRIES;
+		do
+		{
+			*pse_status = sgx_create_pse_session();
+			if (*pse_status != SGX_SUCCESS)
+				return SGX_ERROR_UNEXPECTED;
 		} while (*pse_status == SGX_ERROR_BUSY && retries--);
-		if ( *pse_status != SGX_SUCCESS ) return SGX_ERROR_UNEXPECTED;
+		if (*pse_status != SGX_SUCCESS)
+			return SGX_ERROR_UNEXPECTED;
 	}
 
-	ra_status= sgx_ra_init(&key, b_pse, ctx);
+	ra_status = sgx_ra_init(&key, b_pse, ctx);
 
-	if ( b_pse ) {
-		int retries= PSE_RETRIES;
-		do {
-			*pse_status= sgx_close_pse_session();
-			if ( *pse_status != SGX_SUCCESS ) return SGX_ERROR_UNEXPECTED;
+	if (b_pse)
+	{
+		int retries = PSE_RETRIES;
+		do
+		{
+			*pse_status = sgx_close_pse_session();
+			if (*pse_status != SGX_SUCCESS)
+				return SGX_ERROR_UNEXPECTED;
 		} while (*pse_status == SGX_ERROR_BUSY && retries--);
-		if ( *pse_status != SGX_SUCCESS ) return SGX_ERROR_UNEXPECTED;
+		if (*pse_status != SGX_SUCCESS)
+			return SGX_ERROR_UNEXPECTED;
 	}
 #else
-	ra_status= sgx_ra_init(&key, 0, ctx);
+	ra_status = sgx_ra_init(&key, 0, ctx);
 #endif
 
 	return ra_status;
 }
 
 sgx_status_t enclave_ra_init_def(int b_pse, sgx_ra_context_t *ctx,
-	sgx_status_t *pse_status)
+								 sgx_status_t *pse_status)
 {
 	return enclave_ra_init(def_service_public_key, b_pse, ctx, pse_status);
 }
@@ -168,23 +183,24 @@ sgx_status_t enclave_ra_init_def(int b_pse, sgx_ra_context_t *ctx,
  */
 
 sgx_status_t enclave_ra_get_key_hash(sgx_status_t *get_keys_ret,
-	sgx_ra_context_t ctx, sgx_ra_key_type_t type, sgx_sha256_hash_t *hash)
+									 sgx_ra_context_t ctx, sgx_ra_key_type_t type, sgx_sha256_hash_t *hash)
 {
 	sgx_status_t sha_ret;
 	sgx_ra_key_128_t k;
 
 	// First get the requested key which is one of:
-	//  * SGX_RA_KEY_MK 
+	//  * SGX_RA_KEY_MK
 	//  * SGX_RA_KEY_SK
 	// per sgx_ra_get_keys().
 
-	*get_keys_ret= sgx_ra_get_keys(ctx, type, &k);
-	if ( *get_keys_ret != SGX_SUCCESS ) return *get_keys_ret;
+	*get_keys_ret = sgx_ra_get_keys(ctx, type, &k);
+	if (*get_keys_ret != SGX_SUCCESS)
+		return *get_keys_ret;
 
 	/* Now generate a SHA hash */
 
-	sha_ret= sgx_sha256_msg((const uint8_t *) &k, sizeof(k), 
-		(sgx_sha256_hash_t *) hash); // Sigh.
+	sha_ret = sgx_sha256_msg((const uint8_t *)&k, sizeof(k),
+							 (sgx_sha256_hash_t *)hash); // Sigh.
 
 	/* Let's be thorough */
 
@@ -195,7 +211,7 @@ sgx_status_t enclave_ra_get_key_hash(sgx_status_t *get_keys_ret,
 
 sgx_status_t enclave_ra_close(sgx_ra_context_t ctx)
 {
-        sgx_status_t ret;
-        ret = sgx_ra_close(ctx);
-        return ret;
+	sgx_status_t ret;
+	ret = sgx_ra_close(ctx);
+	return ret;
 }
---
+++ evp-key-to-file-struct.c
@@ -0,0 +1,308 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/ec.h>
+
+#define PUBKEY_H_FILE "Enclave/Pubkey.h"
+
+#define PRVKEY_PEM_FILE "service_provider_private_key.pem"
+
+/* run command
+> gcc -o sp-keygen evp-key-to-file-struct.c -lssl -lcrypto && ./sp-keygen
+*/
+
+static enum _error_type {
+    e_none,
+    e_crypto,
+    e_system,
+    e_api
+} error_type = e_none;
+
+#define SGX_ECP256_KEY_SIZE 32
+
+typedef struct _sgx_ec256_public_t
+{
+    uint8_t gx[SGX_ECP256_KEY_SIZE];
+    uint8_t gy[SGX_ECP256_KEY_SIZE];
+} sgx_ec256_public_t;
+
+//
+void crypto_init()
+{
+    /* Load error strings for libcrypto */
+    // ERR_load_crypto_strings();
+
+    /* Load digest and ciphers */
+    OpenSSL_add_all_algorithms();
+}
+
+void crypto_destroy()
+{
+    EVP_cleanup();
+
+    CRYPTO_cleanup_all_ex_data();
+
+    // ERR_free_strings();
+}
+
+int save_private_key_to_pem(EVP_PKEY *key, const char *filename)
+{
+    FILE *fp = fopen(filename, "wb");
+    if (!fp)
+    {
+        perror("Error opening file");
+        return 0;
+    }
+
+    int ret = PEM_write_PKCS8PrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
+    fclose(fp);
+
+    return ret;
+}
+
+int key_to_sgx_ec256(sgx_ec256_public_t *k, EVP_PKEY *key)
+{
+    EC_KEY *eckey = NULL;
+    const EC_POINT *ecpt = NULL;
+    EC_GROUP *ecgroup = NULL;
+    BIGNUM *gx = NULL;
+    BIGNUM *gy = NULL;
+
+    error_type = e_none;
+
+    eckey = EVP_PKEY_get1_EC_KEY(key);
+    if (eckey == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    ecgroup = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
+    if (ecgroup == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    ecpt = EC_KEY_get0_public_key(eckey);
+
+    gx = BN_new();
+    if (gx == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    gy = BN_new();
+    if (gy == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!EC_POINT_get_affine_coordinates_GFp(ecgroup, ecpt, gx, gy, NULL))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!BN_bn2lebinpad(gx, k->gx, sizeof(k->gx)))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!BN_bn2lebinpad(gy, k->gy, sizeof(k->gy)))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+cleanup:
+    if (gy != NULL)
+        BN_free(gy);
+    if (gx != NULL)
+        BN_free(gx);
+    if (ecgroup != NULL)
+        EC_GROUP_free(ecgroup);
+    return (error_type == e_none);
+}
+
+EVP_PKEY *key_generate()
+{
+    EVP_PKEY *key = NULL;
+    EVP_PKEY_CTX *pctx = NULL;
+    EVP_PKEY_CTX *kctx = NULL;
+    EVP_PKEY *params = NULL;
+
+    error_type = e_none;
+
+    /* Set up the parameter context */
+    pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
+    if (pctx == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    /* Generate parameters for the P-256 curve */
+
+    if (!EVP_PKEY_paramgen_init(pctx))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!EVP_PKEY_CTX_set_ec_paramgen_curve_nid(pctx, NID_X9_62_prime256v1))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!EVP_PKEY_paramgen(pctx, &params))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    /* Generate the key */
+
+    kctx = EVP_PKEY_CTX_new(params, NULL);
+    if (kctx == NULL)
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!EVP_PKEY_keygen_init(kctx))
+    {
+        error_type = e_crypto;
+        goto cleanup;
+    }
+
+    if (!EVP_PKEY_keygen(kctx, &key))
+    {
+        error_type = e_crypto;
+        EVP_PKEY_free(key);
+        key = NULL;
+    }
+
+cleanup:
+    if (kctx != NULL)
+        EVP_PKEY_CTX_free(kctx);
+    if (params != NULL)
+        EVP_PKEY_free(params);
+    if (pctx != NULL)
+        EVP_PKEY_CTX_free(pctx);
+
+    return key;
+}
+
+// main
+int main()
+{
+
+    EVP_PKEY *pkey = NULL;
+    sgx_ec256_public_t public_key;
+
+    crypto_init();
+
+    // genearate key
+    pkey = key_generate();
+
+    // Save private key to PEM file
+    if (!save_private_key_to_pem(pkey, PRVKEY_PEM_FILE))
+    {
+        printf("Error saving private key to PEM file.\n");
+    }
+
+    printf("PRIVATE KEY : service providers private key is saved to %s file successfully.\n", PRVKEY_PEM_FILE);
+
+    // convert key to sgx_ec256_public_t
+    key_to_sgx_ec256(&public_key, pkey);
+
+    // // Print the gx and gy components of the sgx_ec256_public_t
+    // printf("gx: ");
+    // for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+    // {
+    //     printf("0x%02x, ", public_key.gx[i]);
+    // }
+    // printf("\n");
+
+    // printf("gy: ");
+    // for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+    // {
+    //     printf("0x%02x, ", public_key.gy[i]);
+    // }
+    // printf("\n");
+
+    // Example: Print the coordinates in the C-style array format
+    printf("static const sgx_ec256_public_t def_service_public_key = {\n");
+    printf("    {");
+    for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+    {
+        printf("0x%02x", public_key.gx[i]);
+        if (i != SGX_ECP256_KEY_SIZE - 1)
+            printf(", ");
+    }
+    printf("},\n");
+
+    printf("    {");
+    for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+    {
+        printf("0x%02x", public_key.gy[i]);
+        if (i != SGX_ECP256_KEY_SIZE - 1)
+            printf(", ");
+    }
+    printf("}\n};\n");
+
+    // printf("\n\n /* copy this def_service_public_key variable and paste in Enclave.h */");
+
+    // Write the output to Pubkey.h
+    FILE *pubkey_file = fopen(PUBKEY_H_FILE, "w");
+    if (pubkey_file != NULL)
+    {
+        fprintf(pubkey_file, "#ifndef PUBKEY_H\n");
+        fprintf(pubkey_file, "#define PUBKEY_H\n\n");
+        fprintf(pubkey_file, "#include <stdint.h>\n\n");
+        fprintf(pubkey_file, "#include \"sgx_tkey_exchange.h\"\n");
+        fprintf(pubkey_file, "#include \"sgx_tcrypto.h\"\n\n");
+        fprintf(pubkey_file, "static const sgx_ec256_public_t def_service_public_key = {\n");
+        fprintf(pubkey_file, "    {");
+        for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+        {
+            fprintf(pubkey_file, "0x%02x", public_key.gx[i]);
+            if (i != SGX_ECP256_KEY_SIZE - 1)
+                fprintf(pubkey_file, ", ");
+        }
+        fprintf(pubkey_file, "},\n");
+
+        fprintf(pubkey_file, "    {");
+        for (int i = 0; i < SGX_ECP256_KEY_SIZE; i++)
+        {
+            fprintf(pubkey_file, "0x%02x", public_key.gy[i]);
+            if (i != SGX_ECP256_KEY_SIZE - 1)
+                fprintf(pubkey_file, ", ");
+        }
+        fprintf(pubkey_file, "}\n\n};\n\n");
+        fprintf(pubkey_file, "#endif // PUBKEY_H\n");
+        fclose(pubkey_file);
+        printf("PUBLIC KEY : service providers public key is added in %s file with sgx_ec256_public_t def_service_public_key successfully.\n", PUBKEY_H_FILE);
+
+        // fclose(pubkey_file);
+    }
+    else
+    {
+        printf("Error creating %s file.\n", PUBKEY_H_FILE);
+    }
+
+    // Clean up
+    EVP_PKEY_free(pkey);
+
+    //
+    crypto_destroy();
+
+    return 0;
+}
